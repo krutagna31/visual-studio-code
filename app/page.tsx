5d75b45c8b61ff6348e80b73e769c85ccccc63ca
Krutagna Patel <krutagnapatel31@gmail.com>
2025-08-04 10:00:10 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from "@/components/mode-toggle";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>React Shadcn Template</h1>
+      <ModeToggle />
+    </div>
+  );
+}
